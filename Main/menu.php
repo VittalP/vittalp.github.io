c4c160ffbe5437e542d936fa149c41eb6efd4830
Vittal <vittalp88@gmail.com>
2016-03-09 15:51:49 -0500

    push current webpage

---
+++ Main/menu.php
@@ -0,0 +1,4 @@
+<li class="current"><a href="<?php echo $ROOT; ?>index.html">Home</a></li>
+<li><a href="#publications">Publications</a></li>
+<li><a href="#awards">Awards</a></li>
+<li><a href="#contact">Contact</a></li>
---
+++ www/index.php
@@ -0,0 +1,255 @@
+<?php include("./Main/head.php"); ?>
+<body>
+	<div id="content">
+		<p id="top">"If we knew what it was we were doing, it would not be called research, would it?" - Albert Einstein</p>
+		<div id="logo">
+			<h1><a href="<?php echo $ROOT; ?>index.html">Vittal Premachandran</a></h1>
+		</div>
+		<ul id="menu">
+			<?php include("./Main/menu.php"); ?>
+		</ul>
+		<div class="line"></div>
+		<div id="pitch">
+			<img style="float:left;margin:0 75px 0 50px;" src="<?php echo $ROOT; ?>images/vittal1.jpg" height=250px/>
+			<br/>
+			<font size="4"><b>Postdoctoral Research Fellow</b></font>
+			<br/>
+			<font size="6" color="#B20F0F"><b>University of California, Los Angeles</b></font>
+            <br/>
+            <hr/>
+            <br/>
+            <font size="4" color="#000000"><b>Current and Previous Affiliations</b></font>
+            <br/>
+            <br/>
+            &nbsp;&nbsp;&nbsp;
+            <img src="<?php echo $ROOT; ?>images/ucla.jpg" height=75px/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img src="<?php echo $ROOT; ?>images/nus.jpg" height=75px/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <img src="<?php echo $ROOT; ?>images/ntu_logo.jpg" height=75px/>
+            <br/>
+            <font size="2" color="#000000">
+            &nbsp;&nbsp;Postdoctoral Research Fellow&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Postdoctoral Research Fellow&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PhD Student
+            <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(October 2014 - Present)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(October 2013 - August 2014)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(August 2009 - August 2013) 
+            </font>
+		</div>	
+
+		<div class="center">
+		
+			<h3>Brief Bio</h3>
+			
+			<p>Vittal Premachandran is a Postdoctoral Research Fellow at the University of California, Los Angeles. He obtained the B.E. degree in Computer Science from Visveswaraya Technological University, India, in 2009, graduating with a First Class with Distinction. He received his Ph.D degree, also in Computer Science, from Nanyang Technological University, Singapore, in 2014. He has previously worked as a Postdoctoral Research Fellow at the National University of Singapore.</p>
+
+			<p>His research interests span computer vision and machine learning, with a focus on semantic analysis of scenes. His work has received the Best Student Paper award at the IEEE International Conference on Image Processing (ICIP) in 2013.</p>
+			
+		</div>
+		<div class="line"></div>
+		<div class="center">
+		
+			<h3>News</h3>
+			<p>
+			<ul id="list">
+				<li><font color="red">[NEW]</font> Started working as a Postdoctoral Research Fellow at the <a href="http://ccvl.stat.ucla.edu/" target="_blank">Center for Cognition, Vision, and Learning (CCVL)</a>, which is headed by <a href="http://www.stat.ucla.edu/~yuille/" target="_blank">Prof. Alan Yuille</a>, at the University of California, Los Angeles.</li>
+				<li>Selected to participate in the Doctoral Consortium at CVPR 2014!</li>
+				<li>One paper accepted at CVPR 2014!</li>
+				<li>I defended my Ph.D Thesis on 10-Feb-2014</li>
+			</ul>
+			</p>
+		</div>
+
+		<div class="line"></div>
+		<a name="publications" id="publications"></a>
+		<div class="center">
+		<h5 id="pageTop">Research</h5>
+			<h6 id="Publications">Publications</h6>
+			<hr>
+			<p>
+			<ul type="none">
+				<li> <IMG BORDER="0" align="left"width="128" height="90" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/embr.png"/>
+						<b>"Empirical Minimum Bayes Risk Prediction:<br/>
+						How to extract an extra few % performance from vision models with just three more parameters",</b>
+						<br/>
+						Vittal Premachandran, Daniel Tarlow and Dhruv Batra
+						<br/><i>IEEE Conference on Computer Vision and Pattern Recognition (<b>CVPR</b>)</i>, 2014, Columbus, Ohio.
+						<a href="https://filebox.ece.vt.edu/~dbatra/papers/ptb_cvpr14.pdf" target="_blank">[PDF]</a>
+						<br/><br/><br/>
+				</li>
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="75" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/heatmap_icip2013.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala
+						<br/>
+						<b>"What parts of a shape are discriminative?",<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/icip_best_paper_award.JPG" target="_blank">[Best Student Paper Award!]</a></b>
+						<br/>
+						<i>IEEE International Conference on Image Processing (<b>ICIP</b>)</i>, 2013.
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/discriminative_ICIP2013.pdf" target="_blank">[PDF]</a>
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/icip2013talk.pdf" target="_blank">[Presentation Slides]</a>
+						<br/><br/><br/>
+				</li>
+				
+				
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/consensusKNNs.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala
+						<br/>
+						<b>"Consensus of k-NNs for Robust Neighborhood Selection on Graph-Based Manifolds",</b>
+						<br/>
+						<i>IEEE Conference on Computer Vision and Pattern Recognition (<b>CVPR</b>)</i>, 2013, Portland, Oregon.
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/consensus_CVPR2013.pdf" target="_blank">[PDF]</a>
+						<br/><br/><br/><br/><br/>
+				</li>
+				
+				
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/phaseSymmetry.png"/>
+						Ramakrishna Kakarala, Vittal Premachandran* and Prabhu Kaliamoorthi*
+						<br/>
+						<b>"Three-dimensional bilateral symmetry plane estimation in the phase domain",</b>
+						<br/>
+						<i>IEEE Conference on Computer Vision and Pattern Recognition (<b>CVPR</b>)</i>, 2013, Portland, Oregon.
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/symmetryinphase_CVPR2013.pdf" target="_blank">[PDF]</a>
+						<br/>
+						* indicates equal contribution
+						<br/><br/><br/><br/>
+				</li>
+				
+			
+			
+			
+				<li> <IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/ssc.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala,
+						<br/>
+						<b>"Perceptually Motivated Shape Context Which Uses Shape Interiors'',</b>
+						<br/>
+						<i>Pattern Recognition</i>, 2013.
+						<a href="http://arxiv.org/abs/1212.4608" target="_blank">[arXiv]</a>
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/SolidShapeContext_PR2013.pdf" target="_blank">[PDF]</a>
+						<a href="http://www.sciencedirect.com/science/article/pii/S0031320313000691" target="_blank">[DOI]</a>
+						<br/><br/><br/><br/><br/>
+				</li>
+				
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/aestheticsSPIE.jpg"/>
+						Abhishek Agrawal, Ramakrishna Kakarala, Rajesh Somavarapu, Vittal Premachandran
+						<br/>
+						<b>"Can relative skill be determined from a photographic portfolio?'',</b>
+						<br/>
+						<i>SPIE Conference on Electronic Imaging</i>, 2013.
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2013/comparing_SPIE2013.pdf" target="_blank">[PDF]</a>
+						<a href="http://proceedings.spiedigitallibrary.org/proceeding.aspx?articleid=1668831" target="_blank">[SPIE]</a>
+						<br/><br/><br/><br/><br/>
+				</li>
+				
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="80" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/dense.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala,
+						<br/>
+						<b>"Dense sampling of shape interiors for improved representation'',</b>
+						<br/><i>SPIE Conference on Electronic Imaging</i>, 2013.
+						<a href="http://dl.dropbox.com/u/2497281/Papers/2013/denseSampling_SPIE2013.pdf" target="_blank">[PDF]</a>
+						<a href="http://proceedings.spiedigitallibrary.org/proceeding.aspx?articleid=1661853" target="_blank">[SPIE]</a>
+						<br/><br/>
+						<br/><br/>
+				</li>
+				
+				
+						
+				<li> <IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/reflection.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala,
+						<br/>
+						<b>"Improving Shape Context Using Geodesic Information And Refection Invariance'',</b>
+						<br/><i>SPIE Conference on Electronic Imaging</i>, 2013.
+						<a href="http://dl.dropbox.com/u/2497281/Papers/2013/improvingShapeContext_SPIE2013.pdf" target="_blank">[PDF]</a>
+						<a href="http://proceedings.spiedigitallibrary.org/proceeding.aspx?articleid=1568586" target="_blank">[SPIE]</a
+						<br/><br/>
+						<br/><br/><br/><br/>
+				</li>
+				
+				
+						
+				<li>
+						<IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/cic.png"/>
+						Ramakrishna Kakarala, Todd Sachs, and Vittal Premachandran,
+						<br/>
+						<b>"Comparing automated and human ratings of photographic aesthetics'',</b>
+						<br/>
+						2011 Color Imaging Conference, 7-11 Nov, San Jose CA.
+						<a href="http://dl.dropbox.com/u/2497281/Papers/2011/comparingPhotoRatings_CIC2011.pdf" target="_blank">[PDF]</a>&nbsp;<a href="http://www.imaging.org/IST/store/epub.cfm?abstrid=45162" target="_blank">[Web Link]</a><br/><br/>
+						<br/><br/><br/><br/>
+				</li>
+				
+				
+				<li><IMG BORDER="0" align="left"width="128" height="100" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/badpixel.png"/>
+						Vittal Premachandran and Ramakrishna Kakarala,
+						<br/>
+						<b>"Measuring the Effectiveness of Bad Pixel Detection Algorithms Using the ROC Curve",</b>
+						<br/>
+						<i>IEEE Transactions on Consumer Electronics,</i> November 2010.
+						<a href="http://dl.dropbox.com/u/2497281/Papers/2010/measuringEffectiveness_IEEEConsumerElectronics2010.pdf" target="_blank">[PDF]</a>&nbsp;<a href="http://ieeexplore.ieee.org/xpl/freeabs_all.jsp?arnumber=5681135" target="_blank">[IEEE Link]</a>
+						<br/><br/><br/><br/>
+				</li>				
+			</ul>
+			</p>
+		</div>
+		
+
+		<h6 id="Publications">Ph.D Thesis</h6>
+		<hr>
+		<div id="center">
+			
+			<br/>
+			<ul type="none">
+			
+				
+				<li> <IMG BORDER="0" align="left"width="128" height="150" style="margin-right:20px" SRC="<?php echo $ROOT; ?>Research/Images/Thesis_title.png"/>
+						<br/>
+						Vittal Premachandran
+						<br/>
+						<b><i>"Exploiting Shape Properties for Improved Retrieval, Discrimination and Recognition"</i></b>
+						<br/>
+						February 2014.
+						<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2014/Thesis.pdf" target="_blank">[PDF ~12.5MB]</a>
+						<br/>
+						Defense Slides: <a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2014/DefenseSlides.pptx" target="_blank">[PPTx ~14MB]</a>&nbsp;<a href="https://dl.dropboxusercontent.com/u/2497281/Papers/2014/DefenseSlides.pdf" target="_blank">[PDF ~5MB]</a>
+						<br/><br/><br/><br/><br/><br/>
+				</li>
+				
+			</ul>
+		</div>
+
+		<a href="awards" id="awards"></a>
+		<h6 id="awards">Awards</h6>
+		<hr>
+		<div id="center">
+			<ul>
+				<li><b>Best Student Paper Award</b> at the IEEE International Conference on Image Processing (ICIP), 2013, Melbourne, Australia. <a href="http://sce.ntu.edu.sg/News/SuccessStories/Pages/achieves-sch.aspx#ICIP_2013" target="_blank">[NTU-SCE Website]</a></li>
+				<li><b>Best Presentation Award</b> at AOTULE 2011, Beijing. <a href="http://sce.ntu.edu.sg/News/SuccessStories/Pages/achieves-sch2009-11.aspx#sch2011-16" target="_blank">[NTU-SCE Website]</a></li>
+			</ul>
+		</div>
+		<br/>
+
+		<hr>
+
+		<a href="contact" id="contact">
+		<h5 id="pageTop">Contact</h5>
+	
+	<h6>Email</h6>
+	<hr>
+	<div id="center">
+	X@ucla.edu ; X=vittalp<br/>
+	</div>
+
+	<h6>Social Media</h6>
+	<hr>
+	<div style="margin:50px">
+	<a href="https://github.com/VittalP" target="_blank"><img alt="github" src="./images/github.png" height="65px" width="150px"/></a>
+	&nbsp;&nbsp;
+	<a href="http://www.linkedin.com/in/vittalpremachandran" target="_blank"><img alt="LinkedIn" src="./images/linkedin.png" height="75px" width="100px"/></a>
+
+	</div>
+	<div id="moveRight"><a href="<?php echo $ROOT; ?>index.html">[Home]</a></div>
+	<br/>
+
+	<?php include("./Main/footer.php"); ?>
+
+	</div>
+</body>
+</html>
